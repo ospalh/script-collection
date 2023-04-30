faaa184561abe46191cd8f654b50df04d7345444
ospalh <ospalh@gmail.com>
2023-04-30 10:44:47 +0200

    That maybe trovu file, without cherry-picking

---
+++ media/add_entry.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 ; mode: python -*-
+#
+#  © Copyright 2023 Roland Sieker <ospalh@gmail.com>
+#
+# License: GNU AGPL, version 3 or later;
+#  http://www.gnu.org/licenses/agpl.html
+
+
+
+
+import argparse
+import json
+import os
+import re
+import sys
+import yaml
+
+__version__ = '0.0.1'
+
+
+
+def do_process(filename):
+    # No with or other error handling
+    inf = open(filename, 'r')
+    # Do processing here.
+    the_yaml = yaml.safe_load(inf)
+    # add new entry
+    inf.close()
+    ouf = open(filename, 'w')
+    yaml.dump(the_yaml, ouf, allow_unicode=True, sort_keys=False)
+
+
+def add_to_files(file_list, new_file):
+    pass
+
+
+def make_list(raw_list, is_json):
+    if raw_list == '-':
+        stdin_list = sys.stdin.readlines()
+        if len(stdin_list) > 1:
+            return [item.strip() for item in stdin_list]
+        raw_list = stdin_list[0]  # Blow up if empty
+    if is_json:
+        pass
+    else:
+        the_list = raw_list.split()
+    return the_list
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="""Add the entries from the Yaml-file »new« to the files for the namespaces listed in the Json file »namespaces«.""")
+    parser.add_argument("--namespacelist", "-l", type=str, help='''The list of namespaces''')
+    parser.add_argument("--yamlfile", "-y", type=str, help='''The yaml entry to add to the namespaces''')
+    parser.add_argument("--jsonlist", "-j", action='store_true', help='''Whether the entries ar a Json list, not implemented yet''')
+    args = parser.parse_args()
+    nasp_list = make_list(args.namespacelist, args.jsonlist)
+    print(nasp_list)
+    #do_process(args.infile)
